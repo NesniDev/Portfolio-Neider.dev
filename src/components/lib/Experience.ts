f6887d5eb08a62750481afbfbdb1aa9528984cab
Nestnidev <neidernieto@unisangil.edu.co>
2024-05-20 21:46:53 -0500

    split component folder experience.ts

---
+++ src/components/lib/Experience.ts
@@ -0,0 +1,32 @@
+export const WORKS = [
+    {
+      id: 1,
+      title: "Docente de Técnico en Sistemas",
+      subtitle: "IFE COLOMBIA",
+      date: "Febrero 2023 - Actualmente",
+      text: "Diseñé y desarrollé planes de estudio innovadores para el técnico en sistemas, manteniendo el material didáctico actualizado a las nuevas tendencias",
+      text2:
+        "Impartí clases teórico y prácticas sobre temas clave como diseño web, fundamentos de programación (java, python, js), sistemas operativos, Microsoft Office y bases de datos, utilizando métodos pedagógicos efectivos para garantizar una comprensión profunda por parte de los estudiantes",
+      direction: "left",
+    },
+    {
+      id: 2,
+      title: "Freelancer",
+      subtitle: "Diseño de páginas web",
+      date: "Actualmente",
+      text: "Diseñé y desarrollé sitios web personalizados para una variedad de clientes, utilizando tecnologías como HTML, CSS, JS, ASTRO y REACT cumpliendo con las expectativas del cliente y los plazos de entrega.",
+      text2:
+        "Implementé soluciones de diseño responsivo para garantizar una experiencia óptima del usuario en dispositivos móviles y de escritorio.",
+      direction: "right",
+    },
+    {
+      id: 3,
+      title: "Asesor y Cajero",
+      subtitle: "Banco Agrario de Colombia",
+      date: "2017 - 2018",
+      text: "Procesé transacciones de depósito, retiro y transferencia con precisión y eficiencia, manteniendo la integridad financiera",
+      text2:
+        "Fomenté la confianza y la lealtad del cliente a través de interacciones positivas y asesoramiento financiero básico.",
+      direction: "left",
+    },
+  ];
