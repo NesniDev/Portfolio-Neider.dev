a54f2085ee10e9dd4d660843fe812a7308e62cc4
Nestnidev <neidernieto@unisangil.edu.co>
2024-05-20 21:47:15 -0500

    split component folder tecnology.ts

---
+++ src/components/lib/Tecnologys.ts
@@ -0,0 +1,150 @@
+import Html from "@/icons/Html.astro";
+import Tailwind from "@/icons/Tailwind.astro";
+import Css from "@/icons/Css.astro";
+import Bootstrap from "@/icons/Bootstrap.astro";
+import Sass from "@/icons/Sass.astro";
+import React from "@/icons/React.astro";
+import Astro1 from "@/icons/Astro1.astro";
+import GitHub from "@/icons/GitHub.astro";
+import Javascript from "@/icons/Javascript.astro";
+import Typescript from "@/icons/Typescript.astro";
+import NextJs from "@/icons/NextJs.astro";
+import NodeJs from "@/icons/NodeJs.astro";
+import Git from "@/icons/Git.astro";
+import Sql from "@/icons/Sql.astro";
+import Python from "@/icons/Python.astro";
+import Bun from "@/icons/Bun.astro";
+import Pnpm from "@/icons/Pnpm.astro";
+import Notion from "@/icons/Notion.astro";
+import ViteJs from "@/icons/ViteJs.astro";
+import Linux from "@/icons/Linux.astro";
+import MarkDown from "@/icons/MarkDown.astro";
+import Wordpress from "@/icons/Wordpress.astro";
+import Php from "@/icons/Php1.astro";
+import Vercel from "@/icons/Vercel.astro";
+
+export const TOOLS = [
+    {
+      logo: Html,
+      name: "HTML",
+      category: "Lenguaje de Marcado",
+    },
+    {
+      logo: Css,
+      name: "CSS",
+      category: "Lenguaje de hojas de estilo",
+    },
+    {
+      logo: Javascript,
+      name: "Javascript",
+      category: "Lenguaje de programación",
+    },
+  
+    {
+      logo: Tailwind,
+      name: "TAILWIND",
+      category: "Framework de utilidades CSS",
+    },
+    {
+      logo: Astro1,
+      name: "Astro",
+      category: "Framework de desarrollo web",
+    },
+    {
+      logo: React,
+      name: "React",
+      category: "Biblioteca y framework frontend",
+    },
+    {
+      logo: NextJs,
+      name: "NextJs",
+      category: "Framework de desarrollo web",
+    },
+  
+    {
+      logo: Git,
+      name: "Git",
+      category: "Sistema de control de versiones",
+    },
+    {
+      logo: GitHub,
+      name: "GitHub",
+      category: "Plataforma alojamiento código",
+    },
+  
+    {
+      logo: Typescript,
+      name: "Typescript",
+      category: "Lenguajes de programación",
+    },
+    {
+      logo: Sass,
+      name: "SASS",
+      category: "Preprocesador de CSS",
+    },
+    {
+      logo: Bootstrap,
+      name: "Bootstrap",
+      category: "Frameworks de componentes CSS",
+    },
+    {
+      logo: NodeJs,
+      name: "NodeJs",
+      category: "Entorno de ejecución",
+    },
+    {
+      logo: Linux,
+      name: "Linux",
+      category: "Sistema Operativo Linux",
+    },
+    {
+      logo: Sql,
+      name: "Sql Lite",
+      category: "Bases de datos relacionales",
+    },
+    {
+      logo: Python,
+      name: "Python",
+      category: "Lenguaje de programación",
+    },
+    {
+      logo: ViteJs,
+      name: "Vite Js",
+      category: "Empaquetador de JavaScript",
+    },
+    {
+      logo: Notion,
+      name: "Notion",
+      category: "Gestión de proyectos",
+    },
+    {
+      logo: Pnpm,
+      name: "Pnpm",
+      category: "Gestor de dependencias",
+    },
+    {
+      logo: Bun,
+      name: "Bun",
+      category: "Gestor de dependencias",
+    },
+    {
+      logo: MarkDown,
+      name: "MarkDown",
+      category: "Lenguajes de marcado",
+    },
+    {
+      logo: Php,
+      name: "Php",
+      category: "Lenguajes de programación",
+    },
+    {
+      logo: Wordpress,
+      name: "Wordpress",
+      category: "Sistema de gestión de contenido",
+    },
+    {
+      logo: Vercel,
+      name: "Vercel",
+      category: "Alojamiento en la nube",
+    },
+  ];
