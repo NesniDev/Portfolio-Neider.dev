7943438c13a7b59ad5359d6b2197a516e03a4949
Nestnidev <neidernieto@unisangil.edu.co>
2024-05-24 15:27:17 -0500

    update src lib

---
+++ src/lib/Projects.ts
@@ -0,0 +1,39 @@
+import {TECNOLOGYS} from "./Tecnology-Projects"
+export const PROJECTS = [
+    {
+      url: "/img/Lawyer.webp",
+      title: "Page Lawyer",
+      technologies: [
+        TECNOLOGYS.HTML,
+        TECNOLOGYS.CSS,
+        TECNOLOGYS.TAILWIND,
+        TECNOLOGYS.ASTRO,
+        TECNOLOGYS.JAVASCRIPT,
+      ],
+      text: "El objetivo principal era proporcionar una plataforma digital que reflejara la profesionalidad y experiencia del bufete, para desarrollar su presencia en línea mediante la creación de una página web moderna y funcional.",
+      gh: "https://github.com/NesniDev/Page-Lawyer",
+      page: "https://www.abogadasandraalba.com/",
+    },
+    {
+      url: "/img/artex.avif",
+      title: "Page ArtexAlex",
+      technologies: [
+        TECNOLOGYS.HTML,
+        TECNOLOGYS.CSS,
+        TECNOLOGYS.ASTRO,
+        TECNOLOGYS.JAVASCRIPT,
+        TECNOLOGYS.TAILWIND,
+      ],
+      text: "El principal objetivo era crear una plataforma digital que reflejara la creatividad y habilidad artesanal del taller, para fortalecer su presencia en línea mediante la creación de un sitio web moderno y funcional.",
+      gh: "https://github.com/NesniDev/ArtexAlex",
+      page: "https://artexalex.pages.dev/",
+    },
+    {
+      url: "/img/bar.avif",
+      title: "Page Bar",
+      technologies: [TECNOLOGYS.HTML, TECNOLOGYS.CSS, TECNOLOGYS.JAVASCRIPT],
+      text: "El objetivo principal era desarrollar una plataforma digital para el ambiente acogedor del bar, con el fin de fortalecer su presencia en línea mediante la creación de un sitio web moderno y funcional.",
+      gh: "https://github.com/NesniDev/Page-Bar.dev",
+      page: "https://bar-ib.netlify.app/",
+    },
+  ];
