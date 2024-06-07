78fbd9939b08a36eca0d0a63e0effe5ae3e53f1d
Nestnidev <neidernieto@unisangil.edu.co>
2024-06-07 10:52:32 -0500

    Fix atribute class per className

--- src/components/FeaturesFrameworks/Atropos.jsx
+++ src/components/FeaturesFrameworks/Atropos.jsx
@@ -19,22 +19,22 @@ export default function App() {
         rotateXMax={50}
         rotateYMax={50}
       >
-        <div ClassName="image" data-atropos-offset="5">
-          <img ClassName=" rounded-2xl" src="img/imagen.webp " alt="" />
-          <div ClassName="absolute text-center flex flex-col text-xs md:text-xl items-center content-end bottom-0 p-1 md:p-10 mx-auto left-0 right-0 z-10 text-white">
-            <h2 ClassName="text-xl md:text-3xl font-extrabold mb-5">
+        <div className="image" data-atropos-offset="5">
+          <img className=" rounded-2xl" src="img/imagen.webp " alt="" />
+          <div className="absolute text-center flex flex-col text-xs md:text-xl items-center content-end bottom-0 p-1 md:p-10 mx-auto left-0 right-0 z-10 text-white">
+            <h2 className="text-xl md:text-3xl font-extrabold mb-5">
               Ingeniero de Sistemas
             </h2>
-            <p ClassName="text-sm md:text-base font-extralight">
+            <p className="text-sm md:text-base font-extralight">
               "Primero aprende informática y toda la teoría.
             </p>
-            <p ClassName="text-sm md:text-base font-extralight">
+            <p className="text-sm md:text-base font-extralight">
               Después desarrolla un estilo de programación.
             </p>
-            <p ClassName="text-sm md:text-base font-extralight">
+            <p className="text-sm md:text-base font-extralight">
               Entonces, olvídalo todo y hackea"
             </p>
-            <em ClassName="text-sm my-3 md:my-5">"George Carrette"</em>
+            <em className="text-sm my-3 md:my-5">"George Carrette"</em>
           </div>
         </div>
       </Atropos>
--- src/components/FeaturesFrameworks/ButtonForm.jsx
+++ src/components/FeaturesFrameworks/ButtonForm.jsx
@@ -26,7 +26,7 @@ function App() {
         id="button"
         type="submit"
         onClick={alert}
-        class="w-fit m-auto text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:red-cyan-800 rounded-lg text-sm px-5 py-2.5 text-center mb-2 font-extrabold"
+        className="w-fit m-auto text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:red-cyan-800 rounded-lg text-sm px-5 py-2.5 text-center mb-2 font-extrabold"
       >
         {subscribe ? "Enviar Formulario" : "Formulario Enviado"}
       </button>
