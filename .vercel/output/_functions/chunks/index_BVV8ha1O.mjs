75286b88a1c560eeef7df2daf9472fd9eb8ab936
Nestnidev <neidernieto@unisangil.edu.co>
2024-06-07 11:29:36 -0500

    fix to build vercel 1

--- .vercel/output/_functions/chunks/index_BVV8ha1O.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/index_CX8rQwS8.mjs');
-
-export { page };
--- .vercel/output/static/_astro/hoisted.d5SvEsfa.js
+++
@@ -1,1 +0,0 @@
-import"./hoisted.DqR2iBSn.js";const e=document.querySelector("#menu"),n=document.querySelector(".button"),c=document.querySelectorAll("#menu a");n.addEventListener("click",()=>{e.classList.toggle("hidden"),c.forEach(t=>{t.addEventListener("click",()=>{e.classList.add("hidden")})})});
