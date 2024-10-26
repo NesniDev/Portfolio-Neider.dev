89901b102a9bc14846bc11e10606ae4db6790cb2
Neider Nieto <nestnidev17@gmail.com>
2024-10-25 20:21:23 -0500

    Add new project

--- src/lib/Projects.ts
+++ src/lib/Projects.ts
@@ -13,6 +13,7 @@ export const PROJECTS = [
       text: "El objetivo principal era proporcionar una plataforma digital que reflejara la profesionalidad y experiencia del bufete, para desarrollar su presencia en línea mediante la creación de una página web moderna y funcional.",
       gh: "https://github.com/NesniDev/Page-Lawyer",
       page: "https://www.abogadasandraalba.com/",
+      badge: "Completed"
     },
     {
       url: "/img/artex.avif",
@@ -27,6 +28,16 @@ export const PROJECTS = [
       text: "El principal objetivo era crear una plataforma digital que reflejara la creatividad y habilidad artesanal del taller, para fortalecer su presencia en línea mediante la creación de un sitio web moderno y funcional.",
       gh: "https://github.com/NesniDev/ArtexAlex",
       page: "https://artexalex.pages.dev/",
+      badge: "Completed"
+    },
+    {
+      url: "/img/bar.avif",
+      title: "Page View APIs",
+      technologies: [TECNOLOGYS.HTML, TECNOLOGYS.CSS, TECNOLOGYS.JAVASCRIPT, TECNOLOGYS.ASTRO, TECNOLOGYS.TAILWIND],
+      text: "El objetivo principal era desarrollar una plataforma para la visualización de APIS, que he creado durante mi proceso de aprendizaje y que permitirá a los usuarios acceder a la información de manera fácil y sencilla.",
+      gh: "https://github.com/NesniDev/View-Projects-Api",
+      page: "https://view-projects-api.vercel.app/",
+      badge: "In Progress"
     },
     {
       url: "/img/bar.avif",
@@ -35,5 +46,7 @@ export const PROJECTS = [
       text: "El objetivo principal era desarrollar una plataforma digital para el ambiente acogedor del bar, con el fin de fortalecer su presencia en línea mediante la creación de un sitio web moderno y funcional.",
       gh: "https://github.com/NesniDev/Page-Bar.dev",
       page: "https://bar-ib.netlify.app/",
-    },
+      badge: "Completed"
+    }
+    
   ];
