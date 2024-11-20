242dce10667b91f3b52aea7d2c1ed5889bfc6d6f
Neider Nieto <nestnidev17@gmail.com>
2024-11-20 12:01:17 -0500

    config font caviar dreams

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,11 +1,17 @@
 /** @type {import('tailwindcss').Config} */
+import defaultTheme from 'tailwindcss/defaultTheme'
+
 import animations from '@midudev/tailwind-animations'
 
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   darkMode: 'class',
   theme: {
-    extend: {}
+    extend: {
+      fontFamily: {
+        Caviar: ['Caviar Dreams', ...defaultTheme.fontFamily.sans]
+      }
+    }
   },
   plugins: [animations]
 }
