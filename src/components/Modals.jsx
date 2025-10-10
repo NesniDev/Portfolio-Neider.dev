39c5626285a8efae9fe66a1059beebd3ce2c9355
Neider Nieto <nestnidev17@gmail.com>
2025-10-09 20:01:58 -0500

    change style mb

--- src/components/Modals.jsx
+++ src/components/Modals.jsx
@@ -1,13 +1,13 @@
-import { useState } from "react";
-import "./StylesOnCss/Modal.css";
+import { useState } from 'react'
+import './StylesOnCss/Modal.css'
 
 // import { useModal } from "./useModal.jsx";
 
 const Modales = () => {
-  const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false)
 
-  const openModal = () => setIsOpen(true);
-  const closeModal = () => setIsOpen(false);
+  const openModal = () => setIsOpen(true)
+  const closeModal = () => setIsOpen(false)
 
   const CloseMod = () => (
     <svg
@@ -23,16 +23,16 @@ const Modales = () => {
         stroke-linejoin="round"
       ></g>
       <g id="SVGRepo_iconCarrier">
-        {" "}
+        {' '}
         <path
           fill-rule="evenodd"
           clip-rule="evenodd"
           d="M12 22C7.28595 22 4.92893 22 3.46447 20.5355C2 19.0711 2 16.714 2 12C2 7.28595 2 4.92893 3.46447 3.46447C4.92893 2 7.28595 2 12 2C16.714 2 19.0711 2 20.5355 3.46447C22 4.92893 22 7.28595 22 12C22 16.714 22 19.0711 20.5355 20.5355C19.0711 22 16.714 22 12 22ZM8.96965 8.96967C9.26254 8.67678 9.73742 8.67678 10.0303 8.96967L12 10.9394L13.9696 8.96969C14.2625 8.6768 14.7374 8.6768 15.0303 8.96969C15.3232 9.26258 15.3232 9.73746 15.0303 10.0303L13.0606 12L15.0303 13.9697C15.3232 14.2625 15.3232 14.7374 15.0303 15.0303C14.7374 15.3232 14.2625 15.3232 13.9696 15.0303L12 13.0607L10.0303 15.0303C9.73744 15.3232 9.26256 15.3232 8.96967 15.0303C8.67678 14.7374 8.67678 14.2626 8.96967 13.9697L10.9393 12L8.96965 10.0303C8.67676 9.73744 8.67676 9.26256 8.96965 8.96967Z"
           fill="#ff0000"
-        ></path>{" "}
+        ></path>{' '}
       </g>
     </svg>
-  );
+  )
   return (
     <div>
       <button
@@ -85,13 +85,13 @@ const Modales = () => {
                 navegación óptima y una excelente usabilidad.
               </p>
 
-              <p className="text-slate-900 text-[10px] md:text-sm cursor-none italic">
+              <p className="text-center text-slate-900 text-[10px] md:text-sm cursor-none italic mb-5">
                 <i>
-                  Mi objetivo es seguir creciendo como desarrollador frontend,
+                  "Mi objetivo es seguir creciendo como desarrollador frontend,
                   contribuyendo con mis conocimientos y creatividad para
                   construir aplicaciones web que no solo cumplan con los
                   requisitos técnicos, sino que también superen las expectativas
-                  de los usuarios.
+                  de los usuarios.""
                 </i>
               </p>
             </div>
@@ -99,7 +99,7 @@ const Modales = () => {
         </article>
       )}
     </div>
-  );
-};
+  )
+}
 
-export default Modales;
+export default Modales
