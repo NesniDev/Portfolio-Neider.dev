cf176d8d3a8c0d302a33da2e23dfddcf1dc34641
Neider Nieto <nestnidev17@gmail.com>
2025-11-03 23:55:25 -0500

    add new project, and fix img to projects

--- src/lib/Projects.ts
+++ src/lib/Projects.ts
@@ -1,8 +1,23 @@
 import {TECNOLOGYS} from "./Tecnology-Projects"
 export const PROJECTS = [
+  {
+    url: "/img/infoByte.webp",
+    title: "Página de InfoByte",
+    technologies: [
+      TECNOLOGYS.HTML,
+      TECNOLOGYS.CSS,
+      TECNOLOGYS.TAILWIND,
+      TECNOLOGYS.ASTRO,
+      TECNOLOGYS.JAVASCRIPT,
+    ],
+    text: "El objetivo principal era desarrollar una plataforma digital moderna para IFE Colombia, mostrando sus servicios y fortaleciendo su presencia en línea con un diseño atractivo y funcional.",
+    gh: "https://github.com/NesniDev/TecnicoSistemas",
+    page: "https://tecnico-sistemas.vercel.app",
+    badge: "Completed"
+  },
     {
       url: "/img/Lawyer.webp",
-      title: "Page Lawyer",
+      title: "Página de Abogada",
       technologies: [
         TECNOLOGYS.HTML,
         TECNOLOGYS.CSS,
@@ -12,12 +27,12 @@ export const PROJECTS = [
       ],
       text: "El objetivo principal era proporcionar una plataforma digital que reflejara la profesionalidad y experiencia del bufete, para desarrollar su presencia en línea mediante la creación de una página web moderna y funcional.",
       gh: "https://github.com/NesniDev/Page-Lawyer",
-      page: "https://www.abogadasandraalba.com/",
+      page: "https://page-lawyer-nesnidevs-projects.vercel.app",
       badge: "Completed"
     },
     {
       url: "/img/artex.avif",
-      title: "Page ArtexAlex",
+      title: "Página de ArtexAlex",
       technologies: [
         TECNOLOGYS.HTML,
         TECNOLOGYS.CSS,
@@ -32,7 +47,7 @@ export const PROJECTS = [
     },
     {
       url: "/img/API.avif",
-      title: "Page View APIs",
+      title: "Página de visualización de APIs",
       technologies: [TECNOLOGYS.HTML, TECNOLOGYS.CSS, TECNOLOGYS.JAVASCRIPT, TECNOLOGYS.ASTRO, TECNOLOGYS.TAILWIND],
       text: "El objetivo principal era desarrollar una plataforma para la visualización de APIS, que he creado durante mi proceso de aprendizaje y que permitirá a los usuarios acceder a la información de manera fácil y sencilla.",
       gh: "https://github.com/NesniDev/View-Projects-Api",
@@ -41,7 +56,7 @@ export const PROJECTS = [
     },
     {
       url: "/img/bar.avif",
-      title: "Page Bar",
+      title: "Página de Bar",
       technologies: [TECNOLOGYS.HTML, TECNOLOGYS.CSS, TECNOLOGYS.JAVASCRIPT],
       text: "El objetivo principal era desarrollar una plataforma digital para el ambiente acogedor del bar, con el fin de fortalecer su presencia en línea mediante la creación de un sitio web moderno y funcional.",
       gh: "https://github.com/NesniDev/Page-Bar.dev",
@@ -50,8 +65,8 @@ export const PROJECTS = [
     },
     {
       url: "/img/ecommerce.png",
-      title: "Ecommerce",
-      technologies: [TECNOLOGYS.HTML, TECNOLOGYS.CSS, TECNOLOGYS.JAVASCRIPT, TECNOLOGYS.ASTRO, TECNOLOGYS.TAILWIND, TECNOLOGYS.REACT],
+      title: "Página de Ecommerce",
+      technologies: [TECNOLOGYS.REACT, TECNOLOGYS.HTML, TECNOLOGYS.CSS, TECNOLOGYS.JAVASCRIPT, TECNOLOGYS.TAILWIND],
       text: "Este ecommerce fue creado para la empresa de ropa femenina, con el fin de fortalecer su presencia en línea mediante la creación de un sitio web moderno y funcional.",
       gh: "https://github.com/NesniDev/Ecommerce",
       page: "https://ecommerce-products-api.netlify.app",
