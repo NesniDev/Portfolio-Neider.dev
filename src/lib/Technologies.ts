c44cb245a4147cc0a33a11d49197afe5ba266605
Neider Nieto <nestnidev17@gmail.com>
2026-01-15 16:47:54 -0500

    fix tecnologies

---
+++ src/lib/Technologies.ts
@@ -0,0 +1,150 @@
+import Html from '@/icons/Technologies/Html.astro'
+import Tailwind from '@/icons/Technologies/Tailwind.astro'
+import Css from '@/icons/Technologies/Css.astro'
+import Bootstrap from '@/icons/Technologies/Bootstrap.astro'
+import Sass from '@/icons/Technologies/Sass.astro'
+import React from '@/icons/Technologies/React.astro'
+import Astro1 from '@/icons/Technologies/Astro1.astro'
+import GitHub from '@/icons/Technologies/GitHub.astro'
+import Javascript from '@/icons/Technologies/Javascript.astro'
+import Typescript from '@/icons/Technologies/Typescript.astro'
+import NextJs from '@/icons/Technologies/NextJs.astro'
+import NodeJs from '@/icons/Technologies/NodeJs.astro'
+import Git from '@/icons/Technologies/Git.astro'
+import Sql from '@/icons/Technologies/Sql.astro'
+import Python from '@/icons/Technologies/Python.astro'
+import Bun from '@/icons/Technologies/Bun.astro'
+import Notion from '@/icons/Technologies/Notion.astro'
+import ViteJs from '@/icons/Technologies/ViteJs.astro'
+import Linux from '@/icons/Technologies/Linux.astro'
+import MarkDown from '@/icons/Technologies/MarkDown.astro'
+import Wordpress from '@/icons/Technologies/Wordpress.astro'
+import Php from '@/icons/Technologies/Php1.astro'
+import Vercel from '@/icons/Technologies/Vercel.astro'
+import Java from '@/icons/Technologies/Java.astro'
+
+export const TOOLS = [
+  {
+    logo: Html,
+    name: 'HTML',
+    category: 'Lenguaje de Marcado'
+  },
+  {
+    logo: Css,
+    name: 'CSS',
+    category: 'Lenguaje de hojas de estilo'
+  },
+  {
+    logo: Javascript,
+    name: 'Javascript',
+    category: 'Lenguaje de programación'
+  },
+
+  {
+    logo: Tailwind,
+    name: 'TAILWIND',
+    category: 'Framework de utilidades CSS'
+  },
+  {
+    logo: Astro1,
+    name: 'Astro',
+    category: 'Framework de desarrollo web'
+  },
+  {
+    logo: React,
+    name: 'React',
+    category: 'Biblioteca y framework frontend'
+  },
+  {
+    logo: NextJs,
+    name: 'NextJs',
+    category: 'Framework de desarrollo web'
+  },
+
+  {
+    logo: Git,
+    name: 'Git',
+    category: 'Sistema de control de versiones'
+  },
+  {
+    logo: GitHub,
+    name: 'GitHub',
+    category: 'Plataforma alojamiento código'
+  },
+
+  {
+    logo: Typescript,
+    name: 'Typescript',
+    category: 'Lenguajes de programación'
+  },
+  {
+    logo: Sass,
+    name: 'SASS',
+    category: 'Preprocesador de CSS'
+  },
+  {
+    logo: Bootstrap,
+    name: 'Bootstrap',
+    category: 'Frameworks de componentes CSS'
+  },
+  {
+    logo: NodeJs,
+    name: 'NodeJs',
+    category: 'Entorno de ejecución'
+  },
+  {
+    logo: Linux,
+    name: 'Linux',
+    category: 'Sistema Operativo Linux'
+  },
+  {
+    logo: Sql,
+    name: 'Sql Lite',
+    category: 'Bases de datos relacionales'
+  },
+  {
+    logo: Python,
+    name: 'Python',
+    category: 'Lenguaje de programación'
+  },
+  {
+    logo: ViteJs,
+    name: 'Vite Js',
+    category: 'Empaquetador de JavaScript'
+  },
+  {
+    logo: Notion,
+    name: 'Notion',
+    category: 'Gestión de proyectos'
+  },
+  {
+    logo: Java,
+    name: 'Java',
+    category: 'Lenguaje de programación'
+  },
+  {
+    logo: Bun,
+    name: 'Bun',
+    category: 'Gestor de dependencias'
+  },
+  {
+    logo: MarkDown,
+    name: 'MarkDown',
+    category: 'Lenguajes de marcado'
+  },
+  {
+    logo: Php,
+    name: 'Php',
+    category: 'Lenguajes de programación'
+  },
+  {
+    logo: Wordpress,
+    name: 'Wordpress',
+    category: 'Sistema de gestión de contenido'
+  },
+  {
+    logo: Vercel,
+    name: 'Vercel',
+    category: 'Alojamiento en la nube'
+  }
+]
