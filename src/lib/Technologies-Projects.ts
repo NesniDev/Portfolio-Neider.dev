99e55ae4761fabaca998787e238301bda26d026f
Neider Nieto <nestnidev17@gmail.com>
2026-05-22 20:07:03 -0500

    style: Change the page styles and update some components.

--- src/lib/Technologies-Projects.ts
+++ src/lib/Technologies-Projects.ts
@@ -7,52 +7,17 @@ import JavaScript from '@/icons/Technologies/Javascript.astro'
 import Express from '@/icons/Technologies/Express.astro'
 import NodeJS from '@/icons/Technologies/NodeJS.astro'
 import PostgreSQL from '@/icons/Technologies/PostgreSQL.astro'
+import Docke from '@/icons/Technologies/Docke.astro'
 
 export const TECHNOLOGIES = {
-  HTML: {
-    name: 'HTML',
-    class: 'text-[#e34f26]',
-    icon: Html
-  },
-  TAILWIND: {
-    name: 'TAILWIND',
-    class: 'text-[#0ED7B5]',
-    icon: Tailwind
-  },
-  ASTRO: {
-    name: 'ASTRO',
-    class: 'text-[#FF5D01]',
-    icon: Astro1
-  },
-  CSS: {
-    name: 'CSS',
-    class: 'text-[#30a9dc]',
-    icon: Css
-  },
-  JAVASCRIPT: {
-    name: 'CSS',
-    class: 'text-[#ffff00 ]',
-    icon: JavaScript
-  },
-  REACT: {
-    name: 'REACT',
-    class: 'text-[#00D8FF]',
-    icon: React
-  },
-
-  EXPRESS: {
-    name: 'EXPRESS',
-    class: 'text-[#000]',
-    icon: Express
-  },
-  NODEJS: {
-    name: 'NODEJS',
-    class: 'text-[#339933]',
-    icon: NodeJS
-  },
-  POSTGRESQL: {
-    name: 'POSTGRESQL',
-    class: 'text-[#326691]',
-    icon: PostgreSQL
-  },
+  HTML: { icon: Html, name: 'HTML' },
+  TAILWIND: { icon: Tailwind, name: 'Tailwind' },
+  ASTRO: { icon: Astro1, name: 'Astro' },
+  CSS: { icon: Css, name: 'CSS' },
+  JAVASCRIPT: { icon: JavaScript, name: 'JavaScript' },
+  REACT: { icon: React, name: 'React' },
+  EXPRESS: { icon: Express, name: 'Express' },
+  NODEJS: { icon: NodeJS, name: 'NodeJS' },
+  DOCKER: { icon: Docke, name: 'Docker' },
+  POSTGRESQL: { icon: PostgreSQL, name: 'PostgreSQL' }
 }
