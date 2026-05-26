d642b0bfbe516cfa9d1627058627acee29b88ab6
nestnidev <nestnidev17@gmail.com>
2026-05-26 18:11:55 -0500

    feat: initial layout page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,29 @@
 import { defineConfig } from 'astro/config'
-import tailwind from '@astrojs/tailwind'
 import react from '@astrojs/react'
 import sitemap from '@astrojs/sitemap'
 
 import vercel from '@astrojs/vercel'
+import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://neider.dev/',
+
   integrations: [
-    tailwind(),
     react(),
     sitemap({
       filter: (page) => page !== 'https://neider.dev/'
     })
   ],
+
   output: 'server',
+
   adapter: vercel({
     functionPerRoute: false
   }),
-  viewTransitions: true
+
+  viewTransitions: true,
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
 })
