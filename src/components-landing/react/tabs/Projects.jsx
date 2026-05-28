d3e46799677bfeb67f0ff111a851df760e763402
nestnidev <nestnidev17@gmail.com>
2026-05-27 23:18:10 -0500

    feat: initial projects component

---
+++ src/components-landing/react/tabs/Projects.jsx
@@ -0,0 +1,18 @@
+export const Projects = () => {
+  return (
+    <div>
+      <article>
+        <h2>Project 1</h2>
+        <p>Project 1 content</p>
+      </article>
+      <article>
+        <h2>Project 2</h2>
+        <p>Project 2 content</p>
+      </article>
+      <article>
+        <h2>Project 3</h2>
+        <p>Project 3 content</p>
+      </article>
+    </div>
+  )
+}
