e0d2e485f20537c55e9daf73409884f1e76da091
nestnidev <nestnidev17@gmail.com>
2026-05-30 00:34:18 -0500

    feat: Project Reorganization: Separation of Legacy Code and Redesign Structure

--- src/legacy/components/Slider.jsx
+++ src/legacy/components/Slider.jsx
@@ -1,5 +1,5 @@
 import Marquee from 'react-fast-marquee'
-import { TOOLS } from '../lib/Technologies'
+import { TOOLS } from '../../lib/Technologies'
 
 export default function Slider({ speed, gradient, direction, category }) {
   return (
