1d50f17c306f2eaacf1548be2ec186bb745d1284
nestnidev <nestnidev17@gmail.com>
2026-05-30 00:35:46 -0500

    fix: correct information and routes

--- src/lib/Technologies-Projects.ts
+++ src/lib/Technologies-Projects.ts
@@ -1,13 +1,3 @@
-import Html from '@/icons/Technologies/Html.astro'
-import Tailwind from '@/icons/Technologies/Tailwind.astro'
-import Astro1 from '@/icons/Technologies/Astro1.astro'
-import Css from '@/icons/Technologies/Css.astro'
-import React from '@/icons/Technologies/React.astro'
-import JavaScript from '@/icons/Technologies/Javascript.astro'
-import Express from '@/icons/Technologies/Express.astro'
-import NodeJS from '@/icons/Technologies/NodeJS.astro'
-import PostgreSQL from '@/icons/Technologies/PostgreSQL.astro'
-import Docke from '@/icons/Technologies/Docke.astro'
 
 
 export const TECHNOLOGIES = {
